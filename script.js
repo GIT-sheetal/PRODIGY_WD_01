d39d3d33051578a6f40892f3f4bdc142fed33585
GIT-sheetal <iamsheetal17@gmail.com>
2024-06-15 11:41:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.getElementById('navbar');
-    navbar.style.backgroundColor = window.scrollY > 50 ? '#222' : '#333';
+const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#555';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
 });
-
